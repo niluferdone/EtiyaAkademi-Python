480f7781acb48b8132d6ee2ed6068c144a3f3d4e
niluferdone <77413323+niluferdone@users.noreply.github.com>
2022-11-18 16:23:04 +0300

    Add files via upload

---
+++ employees.py
@@ -0,0 +1,23 @@
+#bir şirket çalışanları verilerini tutan dosya oluşturulacak
+#kullanıcıdan çalışan sayısı alınacak 
+# çalışan sayısı kadar isim, soyisim, maaş bilgisi alınacak
+#dosyadaki her satıra "ad soyad-maaş" kalıbında çalışan bilgileri eklenecek
+#eklenen çalışanlar eski verileri silmeden alta eklenecek 
+#hata yakalama işlemleri olacak 
+#programın sonunda bu dosyadan bilgileri satır satır okuyup listeleyecek kod yazılacak 
+
+
+employees = open("employees.txt", "a+")
+try:
+   employeesCount = int(input("çalışan sayısı giriniz: "))
+except:
+    print("düzgün sayı girmediniz.")
+    exit()
+for i in range(employeesCount):
+    employeesName =input(f"{i+1}. çalışanın adını giriniz: ")
+    employeesLastName =input(f"{i+1}. çalışanın soyadını giriniz: ")
+    employeesSalary = float(input(f"{i+1}. çalışanın maaş bilgisini giriniz:"))
+    employees.write(f"{employeesName} {employeesLastName} - {employeesSalary} \n")
+employees.seek(0)
+print(employees.read())
+employees.close()
